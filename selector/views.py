80b39392b00ba14beb0bf214e49a81fcaddbd4d1
Robin James Kerrison <rjk1994@gmail.com>
2018-06-30 11:50:35 +0100

    Using a template for index view

--- selector/views.py
+++ selector/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Bet
 
 def index(request):
   bet_list = Bet.objects.order_by('-id')
-  output = ', '.join(bet for bet in bet_list)
-  return HttpResponse(output)
+  template = loader.get_template('selector/index.html')
+  context = {
+    'bet_list': bet_list
+  }
+  return HttpResponse(template.render(context, request))
 
 def bet(request, bet_id):
   bet = Bet.objects.get(id=bet_id)
