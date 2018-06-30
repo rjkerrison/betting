4aca6eb5a74b1b9badc42cca03e7470970de7c0e
Robin James Kerrison <rjk1994@gmail.com>
2018-06-30 09:49:09 +0100

    Useful string outputs from models

--- selector/models.py
+++ selector/models.py
@@ -5,6 +5,12 @@ class Line(models.Model):
   odds_numerator = models.IntegerField(default = 1)
   odds_denominator = models.IntegerField(default = 1)
 
+  def __str__(self):
+    return f'{self.line_description} at {self.odds_numerator}/{self.odds_denominator}'
+
 class Bet(models.Model):
   line = models.ForeignKey(Line, on_delete=models.CASCADE)
   stake = models.FloatField(default=0.0)
+
+  def __str__(self):
+    return f'{self.stake} on {self.line}'
