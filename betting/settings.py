73c61e49b95370e45cfc3ad558b12187a5297e02
Robin James Kerrison <rjk1994@gmail.com>
2018-06-30 09:02:32 +0100

    Line and Bet models

--- betting/settings.py
+++ betting/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'selector.apps.SelectorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- selector/models.py
+++ selector/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Line(models.Model):
+  line_description = models.CharField(max_length=200)
+  odds_numerator = models.IntegerField(default = 1)
+  odds_denominator = models.IntegerField(default = 1)
+
+class Bet(models.Model):
+  line = models.ForeignKey(Line, on_delete=models.CASCADE)
+  stake = models.FloatField(default=0.0)
