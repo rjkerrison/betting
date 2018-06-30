7ed59e70fea952ab3f3fc26b28918c74d2f5c7fc
Robin James Kerrison <rjk1994@gmail.com>
2018-06-30 11:46:11 +0100

    Index and Bet detail views

--- selector/urls.py
+++ selector/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:bet_id>', views.bet, name='bet')
 ]
--- selector/views.py
+++ selector/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from .models import Bet
 
 def index(request):
-    return HttpResponse('Hello, world. This is the selector page.')
+  bet_list = Bet.objects.order_by('-id')
+  output = ', '.join(bet for bet in bet_list)
+  return HttpResponse(output)
+
+def bet(request, bet_id):
+  bet = Bet.objects.get(id=bet_id)
+  return HttpResponse(f'You\'re looking at Bet #{bet_id}<br />{bet}')
